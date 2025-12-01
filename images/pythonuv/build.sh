8c6ba6fc2fca8ba69e8274e40075f825000ec8be
aazw <3594744+aazw@users.noreply.github.com>
2025-12-01 21:38:15 +0900

    pythonuv向けビルド追加

---
+++ images/pythonuv/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd "${SCRIPT_DIR}"
+
+# imgaes/baseをビルド
+echo "building images/base as ghcr.io/aazw/devcontainers/base:*"
+./../base/build.sh
+
+# imgaes/baseをローカル参照する形で、imgaes/pythonuvをビルド
+echo "" # 空行挿入
+echo "building images/pythonuv with ghcr.io/aazw/devcontainers/base:latest"
+docker build -t devcontainer-pythonuv --progress=plain ./.devcontainer/
